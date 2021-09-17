ec8731313e7732887d150603d7d77c40be765f0e
artevance <agustinus.michael29@gmail.com>
2021-09-17 12:58:40 +0700

    Make FoundationServiceProvider

---
+++ src/Providers/FoundationServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Gus\Foundation\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use LogicException;
+
+class ObserverServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../config/gus_foundation.php' => config_path('gus_foundation.php'),
+        ]);
+    }
+}
