cd0e93192cdb52e5f4c780304742632de60155c6
artevance <agustinus.michael29@gmail.com>
2021-09-17 12:58:33 +0700

    Make gus foundation config

---
+++ config/gus_foundation.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+    'http' => [
+        'request' => [
+            'feature' => [
+                'model' => \App\Models\Auth\Feature::class,
+            ],
+            'widget' => [
+                'model' => \App\Models\Auth\Widget::class,
+            ],
+            'report' => [
+                'model' => \App\Models\Auth\Report::class,
+            ],
+        ],
+    ],
+    'macro' => [
+        'register' => [
+            'blueprints' => true,
+            'rules' => true,
+        ],
+        'kernel' => \App\Macro\Kernel::class,
+        'config' => [
+            'rule' => [
+                'prefix' => 'g',
+            ],
+        ],
+    ],
+    'observer' => [
+        'kernel' => [
+            \App\Observers\Kernel::class
+        ],
+    ],
+    'eloquent' => [
+        'kernel' => [
+            \App\Models\Kernel::class
+        ],
+    ],
+    'log_activity' => env('BSB_LOG_ACTIVITY', true),
+];
